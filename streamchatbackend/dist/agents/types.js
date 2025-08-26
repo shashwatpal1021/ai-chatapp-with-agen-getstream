89370675d97b84f86039aa9b6fdfd1dee10cdee5
shashwatpal1021 <shashwatpal1021@gmail.com>
2025-08-26 22:36:41 +0530

    removed

--- streamchatbackend/dist/agents/types.js
+++
@@ -1,6 +0,0 @@
-export var AgentPlatform;
-(function (AgentPlatform) {
-    AgentPlatform["OPENAI"] = "openai";
-    AgentPlatform["WRITING_ASSISTANT"] = "writing_assistant";
-})(AgentPlatform || (AgentPlatform = {}));
-//# sourceMappingURL=types.js.map
